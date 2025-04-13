7a519591e38dca2d9d42fe9ae602217f86f53a17
Lubos87CZ <167999134+Lubos87CZ@users.noreply.github.com>
2025-04-13 19:09:10 +0200

    Změna na 12 pocet sten

--- OOP_zaklady/Kostka.cs
+++ OOP_zaklady/Kostka.cs
@@ -27,7 +27,7 @@ public class Kostka
     }
     public Kostka() //dalsi konstruktor, tentokrat bez parametru
     {
-        pocetSten = 8;
+        pocetSten = 12;
         random = new Random(); //vytvoreni instance generatoru nahod. cisel
     }
     ///<summary>
